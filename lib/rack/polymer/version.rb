06471d92e46dce36b5f6c0d32bc06046d20c9368
Iain Barnett <iainspeed@gmail.com>
2013-08-27 17:43:02 +0100

    Added partial version file to get Bundler to shut up.

---
+++ lib/rack/polymer/version.rb
@@ -0,0 +1,12 @@
+module Rack
+  class Polymer
+
+    # The version of this library.
+    VERSION = "0.0.1"
+
+    # The version of Polymer it supports
+    POLYMER_VERSION = "0.0.20130711"
+
+
+  end
+end
