7ac0ae5104b247662030c21c9f839f2a47f95e5f
Iain Barnett <iainspeed@gmail.com>
2013-08-27 18:12:08 +0100

    Finally managed to get a date for this version.

--- lib/rack/polymer/version.rb
+++ lib/rack/polymer/version.rb
@@ -7,6 +7,8 @@ module Rack
     # The version of Polymer it supports
     POLYMER_VERSION = "0.0.20130711"
 
-
+    # This is the release date of the Polymer file, it makes an easy "Last-Modified" date for setting the headers around caching.
+    # @todo remember to change Last-Modified with each release!
+    POLYMER_VERSION_DATE = "Thu, 11 Jul 2013 13:51:21 +0100"
   end
 end
